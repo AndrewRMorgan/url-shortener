7110dc698a6d7cb0980e9a0c8db63564df4fc1d9
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-04-14 11:31:41 +1000

    add code for port and http server

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
   "fmt"
   "net/http"
+  "os"
 )
 
 func main() {
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
 
+  http.HandleFunc("/", Router)
+  http.ListenAndServe(":"+port, nil)
 }
 
 func Router(w http.ResponseWriter, r *http.Request) {
