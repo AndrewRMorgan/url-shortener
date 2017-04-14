438c64332b0be8eb84031d26ce766f0863fc6984
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-04-14 11:56:38 +1000

    add mysql package code

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"net/http"
+	"os"
 )
 
 func main() {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-  }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-  http.HandleFunc("/", Router)
-  http.ListenAndServe(":"+port, nil)
+	http.HandleFunc("/", Router)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func Router(w http.ResponseWriter, r *http.Request) {
-  
+
 }
