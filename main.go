0b08930760245231910001d1428ee7c0c43a884c
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-04-18 08:36:06 +1000

    create variables for database, create functions for Start and AddURL routes

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"fmt"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB
+var err error
+
+func init() {
+	db, err = spl.Open("mysql",
+		"user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -14,17 +26,15 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", Router)
+	http.HandleFunc("/", Start)
+	http.HandleFunc("/new/", AddURL)
 	http.ListenAndServe(":"+port, nil)
-
-	db, err := spl.Open("mysql",
-		"user:password@tcp(127.0.0.1:3306)/hello")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 }
 
-func Router(w http.ResponseWriter, r *http.Request) {
+func Start(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AddURL(w http.ResponseWriter, r *http.Request) {
 
 }
