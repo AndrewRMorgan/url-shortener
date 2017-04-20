e716685f0372c8017d3780f41ff6df9bc6273ca8
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-04-20 10:04:59 +1000

    create new mux handlers, create new handler functions, create random number function

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"math/rand"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -28,17 +31,27 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", Router)
-	http.HandleFunc("/new/", AddURL)
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/new/", NewHandler)
+	r.HandleFunc("/new/{url}", CreateURLHandler)
+	r.HandleFunc("/{id}", GetURLHandler)
+	http.Handle("/", r)
+
 	http.ListenAndServe(":"+port, nil)
 }
 
-func Router(w http.ResponseWriter, r *http.Request) {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetURL(w http.ResponseWriter, r *http.Request) {
-	var shortUrl string
+func NewHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetURLHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var shortUrl string = vars["id"]
 	var originalUrl string
 	err = db.QueryRow("SELECT original_url FROM urls WHERE short_url = ?", shortUrl).Scan(&originalUrl)
 	if err != nil {
@@ -48,7 +61,20 @@ func GetURL(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(originalUrl)
 }
 
+func CreateURLHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	originalURL := vars["url"]
+	randNum := random(0, 9999)
+	id := strconv.Itoa(randNum)
+	shortURL := "https://morning-retreat-24523.herokuapp.com/" + id
 
-func AddURL(w http.ResponseWriter, r *http.Request) {
+	res, err := db.Exec("INSERT INTO urls(original_url, short_url) VALUES(?, ?)", originalURL, shortURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max - min) + min
 }
